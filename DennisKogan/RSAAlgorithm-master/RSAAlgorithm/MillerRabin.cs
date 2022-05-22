d6a5dd0722c0ee02ecd47efdbf812de36a644672
Dennis Kogan <kogan.dg@gmail.com>
2022-05-21 18:55:06 -0700

    Add files via upload

---
+++ DennisKogan/RSAAlgorithm-master/RSAAlgorithm/MillerRabin.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace RSAAlgorithm
+{
+    public class MillerRabin
+    {
+        int lowLimit = 100000;
+        List<BigInteger> lowPrimes;
+
+        RNGCryptoServiceProvider rng;
+        Random rand;
+        public MillerRabin()
+        {
+            lowPrimes = new List<BigInteger>();
+
+            rand = new Random();
+            rng = new RNGCryptoServiceProvider();
+
+            for (int i = 2; i <= lowLimit; i++)
+            {
+                if (IsPrimeSlow(i))
+                {
+                    lowPrimes.Add(i);
+                }
+
+            }
+
+        }
+
+        public bool IsPrimeSlow(int num)
+        {
+            if (num < 2)
+            {
+                return false;
+            }
+            else if (num == 2)
+            {
+                return true;
+            }
+            else if (num % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 3; i <= Math.Ceiling(Math.Sqrt(num)); i += 2)
+                {
+                    if (num % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+        }
+
+        public BigInteger GenerateLargePrimeNumber(int bits)
+        {
+            int bytesize = (int)Math.Ceiling(bits / 8.0);
+            byte[] arr = new byte[bytesize];
+
+            rng.GetBytes(arr);
+
+            BigInteger bigInt = BigInteger.Abs(new BigInteger(arr));
+
+            while (!IsPrime(bigInt))
+            {
+                rng.GetBytes(arr);
+                bigInt = BigInteger.Abs(new BigInteger(arr));
+            }
+
+            return bigInt;
+        }
+
+        public bool IsPrime(BigInteger num)
+        {
+            if (num < 2)
+            {
+                return false;
+            }
+            else if (lowPrimes.Contains(num))
+            {
+                return true;
+            }
+
+            foreach (var prime in lowPrimes)
+            {
+                if (num % prime == 0)
+                {
+                    return false;
+                }
+            }
+
+            return RabinMillerTest(num);
+        }
+
+        // returns true if num is a prime number
+        private bool RabinMillerTest(BigInteger num)
+        {
+            BigInteger s = num - 1;
+            int t = 0;
+
+            // keep halving s until it is odd, and use t to count how many times we halve s
+            while (s % 2 == 0)
+            {
+                s /= 2;
+                t += 1;
+            }
+
+            // try to falsify nums primality 5 times
+            for (int trial = 0; trial < 5; trial++)
+            {
+                // generate a number between 2 and num - 1
+                BigInteger a = RandomBigInteger(num - 2) + 1;
+
+                BigInteger v = BigInteger.ModPow(a, s, num);
+
+                if (v != 1)
+                {
+                    int i = 0;
+                    while (v != (num - 1))
+                    {
+                        if (i == t - 1)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            i++;
+
+                            v = BigInteger.ModPow(v, 2, num);
+                        }
+
+                    }
+                }
+
+            }
+
+
+            return true;
+        }
+
+        private BigInteger RandomBigInteger(BigInteger N)
+        {
+            BigInteger result = 0;
+            do
+            {
+                int length = (int)Math.Ceiling(BigInteger.Log(N, 2));
+                int numBytes = (int)Math.Ceiling(length / 8.0);
+                byte[] data = new byte[numBytes];
+                rand.NextBytes(data);
+                result = new BigInteger(data);
+            } while (result >= N || result <= 0);
+            return result;
+        }
+    }
+}
---
+++ DennisKogan/RSAAlgorithm-master/RSAAlgorithm/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+using System.Linq;
+using System.Text;
+
+namespace RSAAlgorithm
+{
+    class Program
+    {
+        static int byteSize = 256; // 256 bits
+
+        static void Main(string[] args)
+        {
+
+            //for (int i = 0; i < 2000; i++)
+            //{
+            //    var factors = PrimeFactorization.GetFactors(i);
+            //    Console.Write($"Num: {i} prime factorization is: ");
+
+            //    foreach(var factor in factors)
+            //    {
+            //        Console.Write($"{factor},");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            //var factors = PrimeFactorization.GetFactors(int.MaxValue / 10);
+
+            //foreach (var item in factors)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            RSAKeyGenerator rsa = new RSAKeyGenerator();
+
+            var pub = rsa.GetPublicKey();
+            var priv = rsa.GetPrivateKey();
+
+            string pubkeyfilename = @"C:\Users\denni\source\repos\RSAAlgorithm\RSAAlgorithm\testpubkey.txt";
+            string privkeyfilename = @"C:\Users\denni\source\repos\RSAAlgorithm\RSAAlgorithm\testprivkey.txt";
+            rsa.SavePublicKey(pubkeyfilename);
+            rsa.SavePrivateKey(privkeyfilename);
+            Console.WriteLine($"Public Key:{Environment.NewLine}{pub}");
+            Console.WriteLine();
+            Console.WriteLine($"Private Key:{Environment.NewLine}{priv}");
+
+
+            string filename = @"C:\Users\denni\source\repos\RSAAlgorithm\RSAAlgorithm\encrypted_file.txt";
+            string mode = "encrypt";
+            int blockSizeInBytes = 128;  // 128 bytes, we gotta make sure that the blocksize is <= than the length of the key
+
+            if (mode == "encrypt")
+            {
+                var message = "what is for lunch?";
+
+                Console.WriteLine($"encrypting and saving to ${filename}");
+
+                if (!File.Exists(pubkeyfilename))
+                {
+                    throw new FileNotFoundException("public key file not found");
+                }
+
+                EncryptAndWriteToFile(filename, pubkeyfilename, message, blockSizeInBytes);
+
+
+                Console.WriteLine("..done");
+
+                mode = "decrypt";
+            }
+
+
+
+            if (mode == "decrypt")
+            {
+                string decryptedText = ReadFromFileAndDecrypt(filename, privkeyfilename);
+
+                Console.WriteLine("Decrypted text");
+                Console.WriteLine(decryptedText);
+
+            }
+        }
+
+        private static string ReadFromFileAndDecrypt(string encryptedFilename, string privkeyfilename)
+        {
+
+            var keyData = File.ReadAllText(privkeyfilename).Split(',');
+            var keyInfo = new
+            {
+                Keysize = int.Parse(keyData[0]),
+                n = BigInteger.Parse(keyData[1]),
+                d = BigInteger.Parse(keyData[2])
+            };
+
+            var encryptedData = File.ReadAllText(encryptedFilename).Split('_');
+            var encryptedInfo = new
+            {
+                MessageLength = int.Parse(encryptedData[0]),
+                Blocksize = int.Parse(encryptedData[1]),
+                EncryptedMessage = encryptedData[2]
+            };
+
+            if (keyInfo.Keysize < encryptedInfo.Blocksize * 8)
+            {
+                throw new Exception("Keysize and blocksize mismatch.");
+            }
+
+            //convert the encrypted message into BigInteger values
+            var encryptedBlocks = new List<BigInteger>();
+
+            var tempblocks = encryptedInfo.EncryptedMessage.Split(',');
+
+            foreach (var block in tempblocks)
+            {
+                encryptedBlocks.Add(BigInteger.Parse(block));
+            }
+
+
+            // decrypt the BigInteger values
+
+            return DecryptMessage(encryptedBlocks, encryptedInfo.MessageLength, keyInfo.n, keyInfo.d, encryptedInfo.Blocksize);
+        }
+
+        private static string DecryptMessage(List<BigInteger> encryptedBlocks, int messageLength, BigInteger n, BigInteger d, int blocksize)
+        {
+
+            var decryptedBlocks = new List<BigInteger>();
+
+            foreach (var block in encryptedBlocks)
+            {
+                decryptedBlocks.Add(BigInteger.ModPow(block, d, n));
+            }
+
+            string decryptedMessage = GetTextFromBlocks(decryptedBlocks, messageLength, blocksize);
+
+            return decryptedMessage;
+        }
+
+        private static string GetTextFromBlocks(List<BigInteger> decryptedBlocks, int messageLength, int blocksize)
+        {
+            StringBuilder message = new StringBuilder();
+
+            for (int j = 0; j < decryptedBlocks.Count; j++)
+            {
+                var block = decryptedBlocks[j];
+
+                var blockMessage = new StringBuilder();
+                for (int i = blocksize - 1; i >= 0; i--)
+                {
+                    if (message.Length + i < messageLength)
+                    {
+                        var asciiValue = BigInteger.Divide(block, BigInteger.Pow(byteSize, i));
+
+                        block = block % BigInteger.Pow(byteSize, i);
+                        blockMessage.Insert(0, (char)asciiValue);
+                    }
+                }
+                message.Append(blockMessage);
+            }
+
+            return message.ToString();
+        }
+
+        // read the public key file and extract the size, n and e from it
+        // encrypt the message and save it to file
+        private static void EncryptAndWriteToFile(string filename, string pubkeyfilename, string message, int blocksize)
+        {
+            var data = File.ReadAllText(pubkeyfilename).Split(',');
+
+            var tempobj = new
+            {
+                Keysize = int.Parse(data[0]),
+                n = BigInteger.Parse(data[1]),
+                e = BigInteger.Parse(data[2])
+            };
+
+            var encryptedBlocks = EncryptMessage(message, tempobj.n, tempobj.e, blocksize);
+
+            StringBuilder encryptedContent = new StringBuilder();
+
+            encryptedContent.AppendJoin(',', encryptedBlocks);
+
+            string output = $"{message.Length}_{blocksize}_{encryptedContent.ToString()}";
+
+            File.WriteAllText(filename, output);
+
+            }
+
+        // Converts the message into a list of block encoded BigIntegers
+        private static List<BigInteger> EncryptMessage(string message, BigInteger n, BigInteger e, int blockSizeInBytes)
+        {
+
+            var encryptedBlocks = new List<BigInteger>();
+
+            var blocks = GetBlocksFromText(message, blockSizeInBytes);
+
+
+            foreach (var block in blocks)
+            {
+                var encryptedBlock = BigInteger.ModPow(block, e, n);
+                encryptedBlocks.Add(encryptedBlock);
+            }
+
+
+            return encryptedBlocks;
+
+        }
+
+        // convert the letters of the message into their ascii values
+        // for the entire message we need to encode the entire blocksize into its corresponding BigInteger
+        // the formula for that is the ascii value * (byteSize raised to i mod blocksize)
+        private static List<BigInteger> GetBlocksFromText(string message, int blockSizeInBytes)
+        {
+            var bytes = new List<byte>();
+
+            foreach (var letter in message)
+            {
+                bytes.Add((byte)letter);
+            }
+
+            var blocks = new List<BigInteger>();
+
+            for (int blockstart = 0; blockstart < bytes.Count; blockstart += blockSizeInBytes)
+            {
+                BigInteger blockint = 0;
+
+                for (int i = blockstart; i < Math.Min(blockstart + blockSizeInBytes, bytes.Count); i++)
+                {
+                    var val = (BigInteger)(bytes[i] * (Math.Pow(byteSize, i % blockSizeInBytes)));
+                    blockint += val;
+                }
+
+                blocks.Add(blockint);
+            }
+
+            return blocks;
+        }
+    }
+}
